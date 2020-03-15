08429b387a58e1c9419d3e78b388e5a0a85b0ea5
AndrewK <kovachik.andrew@gmail.com>
2020-03-14 21:14:59 -0400

    New functions that initiate the the different DE solutions, as well as solving the current step for the non D variables.
    Equations are quite long at the momment and make for difficulty reading.
    May be best to reformat the equations or put the DE as a constant outside
    the class.

--- stellar_properties.py
+++ stellar_properties.py
@@ -1,16 +1,109 @@
+"""
+Class defining a star and it's various differential equations.
+"""
 import numpy as np
 import desolver as de
 
+# m/s^2
+C = 2.98 * 10**8
+#m^3/kg/s^2
+G = 6.6741 * 10**-11
+# m^2*kg/s
+HBAR = 1.055 * 10**-34
+
+Me = 9.11 * 10**-31
+Mp = 1.67 * 10**-27
+#Boltzman J/K
+Kb = 1.381 * 10**-23
+# FIND THIS
+# J/k^4/m^3
+a = 7.566 * 10**-16
+
+
 class Star:
-    stellar_struct = {
-        "density": de.DifferentialEquation("Density"),
-        "temperature1": de.DifferentialEquation("Temperature1"),
-        "temperature2": de.DifferentialEquation("Temperature2"),
-        "mass": de.DifferentialEquation("Mass"),
-        "luminosity": de.DifferentialEquation("Luminosity"),
-        "opticaldepth": de.DifferentialEquation("Optical Depth")
+    """
+    Class definining star. Can calculate many different
+    properties of the star at the various lengths across
+    it's radius
+    """
+
+    def __init__(self, X=0.55, Y=0.43, Z=0.02):
+        """
+        Initializes star by deffining the equations that make up
+        it's stellar structures, and their differential equations"""
+
+        self.X = X
+        self.Y = Y
+        self.Z = Z
+        self.mu = (2 * X + 0.75 * Y + 0.5 * Z)**-1
+        self.properties = {
+            "opacity": 1,
+            "pressure": np.array([]),
+            "energygen": np.array([]),
+            "density": de.DifferentialEquation("Density"),
+            "temperature": de.DifferentialEquation("Temperature"),
+            "mass": de.DifferentialEquation("Mass"),
+            "luminosity": de.DifferentialEquation("Luminosity"),
+            "opticaldepth": de.DifferentialEquation("Optical Depth")
         }
 
-    def __init__(self):
-        return
+        self.setup_sellar_equations()
 
+    def setup_sellar_equations(self):
+        """
+        Assigns the stellar properties their differential equation.
+        Equations are taken from the assignment manual. It is assumed
+        that dd is the derivatives of that stellar property, r is radius
+        and state includes a dictionary of all other stellar properties. 
+        In the case that the property is another DE variable, the first
+        index defines which derivative is grabbed, and the second index 
+        defines the which element to grab
+        """
+        self.properties['luminosity'].set_derivative_relation(
+            lambda dd, r, state: 4 * np.pi * r**2 * state["energy"].val[0, -1])
+
+        self.properties['mass'].set_derivative_relation(
+            lambda dd, r, state: 4 * np.pi * r**2 * state['density'].val[0, -1]
+        )
+
+        self.properties['opticaldepth'].set_derivative_relation(
+            lambda dd, r, state: state['opticaldensity'] * state['density'])
+
+        self.properties['temperature'].set_derivative_relation(
+            lambda dd, r, state: -min(3 * state['opacity'] * state['density'].val[0, -1] * state['luminosity'].val[0, -1] / (16 * np.pi * state['a'] * C * state['temperature'].val[0, -1]**3 * r**2), (1 - 1 / state['gamma']) * state['temperature'].val[0, -1] * G * state['mass'].val[0, -1] * state['density'].val[0, -1] / (state['pressure'].val[0, -1] * r ^ 2))
+        )
+        j
+        self.properties['density'].set_derivative_relation(
+            lambda dd, r, state: -(G * state['mass'].val[0, -1] * state['density'] / r**2 + state['pressure_temp_grad'] * state['temperature'].val[1, -1]) / state['pressure_density_grad']
+        )
+
+    def step_non_de(self):
+        """Updates the variables and arrays of variables that do not depend
+        on differential equations and can be calculated dirrectly.
+        """
+
+        self.properties['pressure'] = np.append(
+            self.properties['pressure'], (3 * np.pi**2)**(2 / 3) * HBAR**2 *
+            (self.properties['density'].val[0, -1] / Mp)**(5 / 3) / (5 * Me) +
+            self.properties['density'] * Kb * self.properties['temperature'] /
+            (self.mu * Mp) + a * self.properties['temperature'].val[0, -1] ^
+            4 / 3)
+
+        self.properties[
+            'pressure_temp_grad'] = self.properties['density'] * Kb / (
+                self.mu * Mp
+            ) + 4 * a * self.properties['temperature'].val[0, -1]**3
+
+        self.properties['pressure_density_grad'] = (3 * np.pi**2)**(
+            2 / 3
+        ) * HBAR**2 * (self.properties['density'].val[0, -1] / Mp)**(2 / 3) / (
+            3 * Me * Mp) + Kb * self.properties['temperature'] / (self.mu * Mp)
+
+        k_es = 0.02 * (1 + self.X)
+        k_ff = 1 * 10**24 * (self.Z + 0.0001) * (
+            self.properties['density'].val[0, -1] / 10**3)**0.7 * (
+                self.properties['temperature'].val[0, -1])**-3.5
+        k_H = 2.5 * 10**-32 * (self.Z / 0.02) * (
+            self.properties['density'].val[0, -1] / 10**3)**0.5 * (
+                self.properties['temperature'].val[0, -1])**9
+        self.properties['opacity'] = (1 / k_H + 1 / max(k_es, k_ff))**-1
