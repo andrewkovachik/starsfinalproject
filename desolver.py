90e04a7379041714718cff8ea38de65c2bf38aa0
AndrewK <kovachik.andrew@gmail.com>
2020-03-18 11:17:19 -0400

    Further removed other functions which seem like the will not need to be implemented. Improved code documentations and hopefully clarity.

--- desolver.py
+++ desolver.py
@@ -1,70 +1,29 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Enabling using LaTex for making labels pretty, not really necessary
-from matplotlib import rc
-rc('text', usetex=True)
-
-PLANCK_CONS = 6.626*10**-34
-SPEED_LIGHT = 2.998*10**8
-BOLTZ_CONS = 1.381*10**-23
-
 def si_prefix_multiple(value, prefix):
+    """
+    Conversion function for different SI prefixes"""
     si_prefixes = {
-            'P':10**15,
-            'T':10**12,
-            'G':10**9,
-            'M':10**6,
-            'k':10**3,
-            'h':10**2,
-            'da':10**1,
-            '0':10**0,
-            'd':10**-1,
-            'c':10**-2,
-            'm':10**-3,
-            'u':10**-6,
-            'n':10**-9,
-            'p':10**-12,
-            'f':10**-15}
+        'P': 10**15,
+        'T': 10**12,
+        'G': 10**9,
+        'M': 10**6,
+        'k': 10**3,
+        'h': 10**2,
+        'da': 10**1,
+        '0': 10**0,
+        'd': 10**-1,
+        'c': 10**-2,
+        'm': 10**-3,
+        'u': 10**-6,
+        'n': 10**-9,
+        'p': 10**-12,
+        'f': 10**-15
+    }
 
     return value * si_prefixes[prefix]
 
-def newtons_method(func, x, dx=0.0001, dylimit=0.000001, iter_limit=100,
-                   show_hist=False):
-    """
-    Newtons method for solving a single variable equation that is equal to zero.
-
-   func(lambda): A single variable lambda function that needs `x` as an input
-    x(float): Initial guess to the solution of the function
-    dx(float): The small step used to calculate the derivatieve of `func`
-    dylimit(float): How close to zero is close enough to end loop
-    iter_limit(int): How many iterations before forcing stop
-    show_hist(Bool): Print history of steps to screen upon completion
-
-    """
-    history = [x]
-    for iter in range(0, iter_limit):
-        slope = (func(x+dx)-func(x))/dx
-        x = x - func(x)/slope
-        y_value = func(x)
-        history += x
-
-        if np.abs(y_value) <= dylimit:
-            if history:
-                print(history)
-            return x
-
-        elif iter > iter_limit:
-            print("Exceded iteration limit")
-            return x
-
-
-def numerical_integration(x_val, y_val):
-
-    dx = x_val[1:] - x_val[0:-1]
-    dydx = y_val[:-1]*dx
-    integral = np.sum(dydx)
-    return integral
 
 class DifferentialEquation:
     """Alows DE's and boundary conditions to be entered
@@ -75,17 +34,20 @@ class DifferentialEquation:
         """
         Print out useful information for debugging
         """
-        info = "\t".join("Derivative {}: {:0.4f}".format(n,i) for n, i in enumerate(self.val[:,-1])) 
+        info = "\t".join("Derivative {}: {:.2e}".format(n, i)
+                         for n, i in enumerate(self.val[:, -1]))
 
         return info
+
     def __repr__(self):
         """
         Print out useful information for debugging
         """
-        info = "\t".join("Derivative {}: {:0.3f}".format(n,i) for n, i in enumerate(self.val[:,-1])) 
+        info = "\t".join("Derivative {}: {:.2e}".format(n, i)
+                         for n, i in enumerate(self.val[:, -1]))
 
         return info
-    
+
     def __init__(self, name="DE Solver"):
         """
         Sets initial values
@@ -93,10 +55,14 @@ class DifferentialEquation:
         x_val (nd.array): x values used to calculate DE
         """
         self.name = name
+        self.boundaries = []
+        self.val = []
+        self.de_relation = None
+        self.step = []
 
     def set_boundaries(self, boundary_cond):
         """
-        Sets the boundary conditions to the given inputs. Boundary 
+        Sets the boundary conditions to the given inputs. Boundary
         conditions are assumed to be for all orders excluding
         the highest order term.
         Args:
@@ -104,40 +70,62 @@ class DifferentialEquation:
         """
         self.boundaries = boundary_cond + [0]
         self.val = np.array(self.boundaries).reshape(-1, 1)
-  
+
     def set_derivative_relation(self, differential_equation):
         """
         Sets the equation used to solve DE. The lambda must take
         in a list of differential forms first and ten the x values
-        
-        differential_equation (lambda): should be of the form lambda dd, x: ...
+
+        Args:
+            differential_equation (lambda dd, x, state_var):
         """
         self.de_relation = differential_equation
 
     def highest_order_boundary(self, x_val, state_vars):
         """
         Calculates the highest order boundary for the case that it is not known
+
+        Args:
+            x_val (float): Current x_value or equivilant
+            state_vars (list or dict): Callable by a lambda function
         """
 
-        self.boundaries[-1] = self.de_relation(
-                self.boundaries, x_val, state_vars)
+        self.boundaries[-1] = self.de_relation(self.boundaries, x_val,
+                                               state_vars)
 
         self.val = np.array(self.boundaries).reshape(-1, 1)
 
-    def solve_differential_step(self, x_val, step_size, state_vars):
+    def solve_differential_step(self,
+                                x_val,
+                                step_size,
+                                state_vars,
+                                auto_add=True):
         """Calculates the next highest order derivative from a set
         of inputed lower order derivatives using the differential
         equation
 
         x_val (float): value of x to use at this step
+        step_size (float): delta x used to step forward
+        state_vars (list or dict): callable by state vars
+        auto_add (bool): Whether to immediately add the small step forward,
+            it's okay to add immediately if it is an uncoupled DE. If they are
+            coupled it should be stepped forward afterwards by itself.
         """
 
-        step = np.array(list(reversed(
-            [0]+[step_size*derivative
-                for derivative in reversed(self.val[1:,-1])]
-            )))
-        step = step+self.val[:,-1]
-        step[-1] = self.de_relation(step, x_val, state_vars)
+        self.step = np.array(
+            list(
+                reversed([0] + [
+                    step_size * derivative
+                    for derivative in reversed(self.val[1:, -1])
+                ])))
+        self.step = self.step + self.val[:, -1]
+        self.step[-1] = self.de_relation(self.step, x_val, state_vars)
 
+        if auto_add:
+            self.add_differential_step()
 
-        self.val  = np.append(self.val, step.reshape(-1,1), axis=1)
+    def add_differential_step(self):
+        """
+        Adds the small step as a new set of value to the outvalues
+        """
+        self.val = np.append(self.val, self.step.reshape(-1, 1), axis=1)
