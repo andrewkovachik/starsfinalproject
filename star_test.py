f291ee3a896dfd93c882dae4efcc828f61dcb86b
AndrewK <kovachik.andrew@gmail.com>
2020-03-27 12:14:15 -0400

    Changes to make testing a little easier.

--- star_test.py
+++ star_test.py
@@ -8,6 +8,7 @@ import numpy as np
 import stellar_properties as star
 import Use_Data as data
 
+
 def example_star(starname=""):
     """
     Creates an example star, prints out some of the steps as it solves
@@ -15,20 +16,18 @@ def example_star(starname=""):
     desired values to show that it works as expected
     """
 
-    expected_radii = 300000000
-    steps = 100
-    step_size = expected_radii / steps
+    steps = 400
+    step_size = 100
 
     sun_like_star = star.Star(
         step_size=step_size,
         X=0.55,
         Y=0.43,
         Z=0.02,
-        cent_density=162200,
-        cent_opticaldepth=2/3,
-        cent_temperature=1.5*10**7,
-        cent_radii=0.01
-        )
+        cent_density=58556,
+        cent_opticaldepth=2 / 3,
+        cent_temperature=8.23544e6,
+        cent_radii=0.01)
 
     radii_steps = np.arange(steps)
     for radii_step in radii_steps:
@@ -42,21 +41,24 @@ def example_star(starname=""):
     # This nixt line is needed for saving data to a text file
     array2D = [[] for i in range(len(items))]
     for item in items:
-        array2D[n] = sun_like_star.properties[item].val[0, :-1]
+        array2D[n] = sun_like_star.properties[item].val[0, :] / max(
+            sun_like_star.properties[item].val[0, :])
         plt.plot(
-            radii_steps * step_size / 696342000, # Units of solar radius
-            array2D[n], # Plots the value of item
+            sun_like_star.properties['radius'] /
+            696342000,  # Units of solar radius
+            array2D[n],  # Plots the value of item
             label=item)
-        plt.xlabel("Solar Radii")
-        plt.title(item)
-        plt.savefig("%s.png" % (item))
-        plt.close()
-        n += 1
+    plt.xlabel("Solar Radii")
+    plt.legend()
+    plt.savefig("RelativeAmps.png")
+    plt.close()
+    n += 1
 
     # Get user decision on saving star data
     save = input("Save this star? (y/n): ")
     # Save the data to a text file
     if save == 'y': data.array2D2txt(array2D, items, starname)
+
 
 if __name__ == '__main__':
     example_star()
