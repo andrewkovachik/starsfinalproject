f1c4cba3dda68613c1bd6915221cc11c946d42e7
EvTokyoko <62354753+EvTokyoko@users.noreply.github.com>
2020-04-01 16:25:13 -0400

    Not done but for ryan to see

--- Bisection.py
+++ Bisection.py
@@ -4,43 +4,82 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+
 sigma = 5.6703*10**-8
 
-# radius_surface = 6900000000
-# temperature_surface = 56000 
-# energygen_centre = 0.001
-# x = np.linspace(0, 100000, 10000)
-# y = [0.0]*10000
-# for i in range(len(x)):
-# 	y[i] = 	(4/3)*np.pi*(radius_surface**3)*x[i]*energygen_centre - (4.0*np.pi*sigma*(radius_surface**2)*(temperature_surface**4))/(np.sqrt(4.0*np.pi*sigma*(radius_surface**2)*(temperature_surface**4)*(4/3)*np.pi*(radius_surface**3)*i*energygen_centre))
-# plt.plot(x,y)
-# plt.show()
+def Lum_error(star):
 
-
-# Surface Temperature = 
-# Central Density = 74237.74616514979
-# Radius = 562477594.72664583
-# Mass = 0.70906952065099615
-# Luminosity = 4.6751081103682353e+25
-
-
-
-def Lum_error(density_centre):
-
-	radius_surface = 562477594.72664583
-	temperature_surface = 3794.8714590341042
-	energygen_centre = 0.001
-
-	L_star = (4/3)*np.pi*(radius_surface**3)*density_centre*energygen_centre
+	radius_surface = star.properties["radius"][-1]
+	temperature_surface = star.properties["temperature"].data(0)[-1]
+	L_star = star.properties["luminosity"].data(0)[-1]
 	return L_star - (4.0*np.pi*sigma*(radius_surface**2)*(temperature_surface**4))/(np.sqrt(4.0*np.pi*sigma*(radius_surface**2)*(temperature_surface**4)*L_star))
 
-def Bisection(function, initial_guess):
+#####inside of make_star after star.solve, array2D[0] = star.properties['radius']######
 
-	print(optimize.bisect(function, initial_guess, initial_guess + (initial_guess/10)))
+"""
+Takes in intial guesses for central temperature and pressure
+as well as the core type and uses them to create a star and save
+a text file.
+"""
+import stellar_properties as starprop
+import Use_Data as data
 
+def make_star(central_temperature, central_density, core_type, name):
 
-#print(optimize.bisect(Lum_error(), 10.0**12, 6500 + (10.0**12/2)))
+	rho_c = central_density
+	rho_c_low = rho_c - 0.1*(rho_c)
+	rho_c_high = rho_c + 0.1*(rho_c)
+	tolerance = 0.001
+	i += 1
 	
+	while ((rho_c_high - rho_c_low) / 2.0) > tolerance:
 
-# Lum_error(69000000,5700,40000,10**12)
-Bisection(Lum_error, 74237.74616514979)
+		rho_c = central_density
+		rho_c_low = rho_c - 0.1*(rho_c)
+		rho_c_high = rho_c + 0.1*(rho_c)
+		tolerance = 0.001
+		i += 1
+
+	    print(central_temperature, rho_c, core_type)
+	    star = starprop.Star(
+	        cent_density=float(rho_c),
+	        cent_temperature=float(central_temperature),
+	        core=core_type,
+	        name=name)
+
+	    star.solve()
+        
+	    if Lum_error(rho_c_low) * Lum_error(rho_c) < 0: #RYAN PLEASE FIX THESE CONDITIONS
+	        rho_c_high = rho_c
+	    if Lum_error(rho_c_low) < Lum_error(rho_c_high):
+	    	rho_c_high = rho_c
+	    if Lum_error(rho_c_high) < Lum_error(rho_c_low):
+	    	rho_c_low = rho_c
+	    else :
+	        rho_c_low = rho_c
+	    if i > 30:
+	        break
+	    rho_c = (rho_c_low + rho_c_high) / 2.0
+
+	save_variable = [
+	        'opticaldepth', 'temperature', 'density', 'luminosity', 'mass',
+	        'opticaldepth_deriv', 'temperature_deriv', 'density_deriv',
+	        'luminosity_deriv', 'mass_deriv', "k_es", "k_ff", "k_h", "opacity",
+	        "pressure", "pressure_temp_grad", "pressure_density_grad", "energy_pp",
+	        "energy_cno", "energy_He", "energy_C", "energygen"
+	    ]
+
+    print("Saving star:", name)
+    deriv = 0
+    array2D = [[] for i in range(len(save_variable) + 1)]
+    array2D[0] = star.properties['radius']
+
+    for index, variable in enumerate(save_variable):
+        if "_deriv" in variable:
+            derive = 1
+            variable = variable.replace("_deriv", "")
+        array2D[index + 1] = star.properties[variable].data(deriv)
+
+    print("Writing star:", name)
+    data.array2D2txt(array2D, ["radius"] + save_variable, name)
+
