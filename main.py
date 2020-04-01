8ba9ada7d6b2bf501e36814ab2d0d1e642807e5e
AsteroidDown <jasper@stahmer.ca>
2020-04-01 18:17:29 -0400

    Added some whitespace when printing star data

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ def main(args):
     file_lines = [file for file in file_lines if '#' not in file]
     for line in file_lines:
         line = line.replace("\n","").split(", ")
+
+        print()
         print(line)
-        name = "Tc_{:.2e}_rhoc_guess{:.2e}_Core_{}".format(
+        name = "Tc_{:.2e}_rhoc_guess{:.2e}_Core_{}_Type_{}".format(
                 float(line[0]), float(line[1]), line[2], line[3])
 
         make_star(line[0], line[1], line[2], name)
+        print()
 
 if __name__ == '__main__':
     parser = arg.ArgumentParser(description = "Plots Stars!")
